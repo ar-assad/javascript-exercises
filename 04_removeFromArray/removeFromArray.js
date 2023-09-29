5c3d616823e7c4e45a12ce03de6bb09599a4d124
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-09-29 15:36:32 +0500

    Solve 01-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr) {
+  for (let i = 1; i < arguments.length; i++) {
+    const index = arr.indexOf(arguments[i]);
+    if (index != -1) {
+      arr.splice(index, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (typeof num1 != 'number' || typeof num2 != 'number') {
+    return 'ERROR';
+  }
+  if (num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
+  return ((Math.abs(num1 - num2) + 1) / 2) * (num1 + num2);
 };
 
 // Do not edit below this line
