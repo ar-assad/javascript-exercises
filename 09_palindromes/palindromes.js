a870dd87670e5c464d2787afdbd31bc09775ad28
abdur Rehman <73696933+ar-assad@users.noreply.github.com>
2023-09-30 16:36:40 +0500

    Solve 08-12

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let chars = str.split('');
+    chars = chars.filter(char => /[A-Za-z0-9]/.test(char));
+    const len = chars.length;
+    for (let i = 0; i < Math.floor(len) / 2; i++) {
+        if (chars[i].toLowerCase() != chars[len - 1 - i].toLowerCase())
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function getAge(person) {
+    if ('yearOfDeath' in person) {
+        return person.yearOfDeath - person.yearOfBirth;
+    }
+    const currYear = new Date().getFullYear();
+    return currYear - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (getAge(person) > getAge(oldest))
+            return person;
+        else   
+            return oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
